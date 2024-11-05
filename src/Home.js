b10a2c9012aee1ae06a75b6975c208e2f4bfca10
Adrián Javorček <59730272+adrian-13@users.noreply.github.com>
2024-11-05 10:40:55 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -114,13 +114,16 @@ function Home() {
     };
   }, [blockNumber]);
 
+
+  // Search panel 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
   const handleInputSearch = () => {
     const input = inputValue.trim();
-
+  
+    // Check if the input is numeric, indicating a block number
     if (isNumeric(input)) {
       window.location.href = `/block/${input}`;
     } else {
@@ -128,20 +131,21 @@ function Home() {
         setErrorMessage(
           "You did not enter any input. Please provide the required data and try again."
         );
-        setShowError(true); // Show error message
-      } else if (input.length === 42) {
-        console.log("Address: ", input);
-        // TODO
-      } else if (input.length === 66) {
-        console.log("Hash: ", input);
-        window.location.href = `/block/${blockNumber}/transactions/${input}`;
+        setShowError(true);
+      } else if (input.length === 42 && input.startsWith("0x")) {
+        // Redirect to the address page
+        window.location.href = `/address/${input}`; // Redirecting to address page
+      } else if (input.length === 66 && input.startsWith("0x")) {
+        // Redirect to the transaction details page
+        window.location.href = `/transaction/${input}`; // Ensure this path matches your routing
       } else {
         setErrorMessage(
           "The entered format is incorrect. Please check your input and try the search again."
         );
-        setShowError(true); // Show error message
+        setShowError(true);
       }
     }
+  
     // Hide the error message after 3 seconds
     if (timerRef.current) {
       clearTimeout(timerRef.current);
@@ -150,6 +154,7 @@ function Home() {
       setShowError(false);
     }, 3000);
   };
+  
 
   const isNumeric = (input) => {
     return /^\d+$/.test(input);
